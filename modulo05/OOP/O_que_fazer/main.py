6d26cfc3df6e54f233930699549002ee495a7173
David Simas <david.Simas81@gmail.com>
2022-11-09 22:01:00 -0300

    Incrementando main.py

--- modulo05/OOP/O_que_fazer/main.py
+++ modulo05/OOP/O_que_fazer/main.py
@@ -3,5 +3,6 @@ from conta import Conta
 print("-" * 30, "Inicio menu", "-" * 30)
 
 conta = Conta(input("Digite o seu nome: "),
-          int(input("Digite o número da conta: "))
-          )
+          int(input("Digite o número da conta: ")),
+        float(input("Digite o saldo da conta: ")),
+        float(input("Digite o limite da conta: ")))
